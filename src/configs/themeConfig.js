0972eef7fe63ea7239fd52a560c5a4378fbac96f
Virak <viirak@gmail.com>
2020-04-15 09:44:26 +0700

    config theme vars

--- src/configs/themeConfig.js
+++ src/configs/themeConfig.js
@@ -3,11 +3,11 @@
 //Template config options
 const themeConfig = {
   layout: "vertical", // options[String]: "vertical"(default), "horizontal"
-  theme: "light", // options[String]: 'light'(default), 'dark', 'semi-dark'
+  theme: "semi-dark", // options[String]: 'light'(default), 'dark', 'semi-dark'
   sidebarCollapsed: false, // options[Boolean]: true, false(default)
-  navbarColor: "default", // options[String]: default / primary / success / danger / info / warning / dark
+  navbarColor: "primary", // options[String]: default / primary / success / danger / info / warning / dark
   navbarType: "floating", // options[String]: floating(default) / static / sticky / hidden
-  footerType: "static", // options[String]: static(default) / sticky / hidden
+  footerType: "hidden", // options[String]: static(default) / sticky / hidden
   disableCustomizer: true, // options[Boolean]: true, false(default)
   hideScrollToTop: false, // options[Boolean]: true, false(default)
   menuTheme: "primary", // options[String]: primary / success / danger / info / warning / dark
