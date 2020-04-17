a2bbdd9d284402c001f82625e2c3229b2777bc12
Virak <viirak@gmail.com>
2020-04-17 23:59:40 +0700

    added translations for page title

--- src/layouts/components/navbar/NavbarTitle.js
+++ src/layouts/components/navbar/NavbarTitle.js
@@ -1,14 +1,15 @@
 import React from "react"
+import { FormattedMessage } from "react-intl"
 
 const pathTitle = {
-  "/": "Home",
-  "/messages/sent": "Sent",
-  "/messages/draft": "Draft",
-  "/messages/telegram/text": "Telegram Text",
-  "/messages/telegram/poll-survey": "Telegram Poll",
-  "/messages/messenger/text": "Messenger Text",
-  "/messages/messenger/poll-survey": "Messenger Poll",
-  "/messages/sms/text": "SMS Text"
+  "/": <FormattedMessage id="Dashboard" />,
+  "/messages/sent": <FormattedMessage id="Sent" />,
+  "/messages/draft": <FormattedMessage id="Draft" />,
+  "/messages/telegram/text": <FormattedMessage id="Telegram Text" />,
+  "/messages/telegram/poll-survey": <FormattedMessage id="Telegram Poll" />,
+  "/messages/messenger/text": <FormattedMessage id="Messenger Text" />,
+  "/messages/messenger/poll-survey": <FormattedMessage id="Messenger Poll" />,
+  "/messages/sms/text": <FormattedMessage id="SMS Text" />
 }
 
 class NavbarTitle extends React.PureComponent {
