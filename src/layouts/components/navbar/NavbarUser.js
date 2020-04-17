ae4f617a056d73125ac9fcc4a19957d2fb3e044d
Virak <viirak@gmail.com>
2020-04-17 11:56:35 +0700

    added display role

--- src/layouts/components/navbar/NavbarUser.js
+++ src/layouts/components/navbar/NavbarUser.js
@@ -120,7 +120,7 @@ class NavbarUser extends React.PureComponent {
               <span className="text-bold-600">
                 {this.props.userName}
               </span>
-              {/* <span className="user-status">Available</span> */}
+              <span className="user-status">Admin</span>
             </div>
             <span data-tour="user">
               <img
