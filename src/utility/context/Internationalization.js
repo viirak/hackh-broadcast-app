c8fdc74a68bdb6785652b42df52683c0a0338bcd
Virak <viirak@gmail.com>
2020-04-18 21:55:47 +0700

    remove unused language files

--- src/utility/context/Internationalization.js
+++ src/utility/context/Internationalization.js
@@ -4,16 +4,10 @@ import Cookie from 'js-cookie'
 
 import messages_en from "../../assets/data/locales/en.json"
 import messages_kh from "../../assets/data/locales/kh.json"
-import messages_de from "../../assets/data/locales/de.json"
-import messages_fr from "../../assets/data/locales/fr.json"
-import messages_pt from "../../assets/data/locales/pt.json"
 
 const menu_messages = {
   en: messages_en,
   kh: messages_kh,
-  de: messages_de,
-  fr: messages_fr,
-  pt: messages_pt
 }
 
 const Context = React.createContext()
