2c1c0305866c26fa648b1409c7c441a7b8ad16da
Virak <viirak@gmail.com>
2020-04-19 22:55:31 +0700

    improve telegram simulator message display

--- src/components/@hackh/PhoneSimulator/phoneSimulator.js
+++ src/components/@hackh/PhoneSimulator/phoneSimulator.js
@@ -1,59 +1,43 @@
 import React from 'react';
-import ScrollContainer from 'react-indiana-drag-scroll'
+// import ScrollContainer from 'react-indiana-drag-scroll'
+import "../../../assets/scss/components/simulator.scss";
 
 const PhoneSimulator = ({ className, type, messages, options = [], image }) => {
-  let socialType;
-  switch (type) {
-    case 'telegram': socialType = { background: '#19B7EA', bubbleBackground: '#fff', optionBackground: '#dadddc' }; break;
-    case 'messenger': socialType = { background: '#f1f0f0', bubbleBackground: '#d3d3d3', optionBackground: '#00B2FF' }; break;
-    default: socialType = { background: '#fff', bubbleBackground: '#d3d3d3', optionBackground: '#00B2FF' }; break;
-  }
-
+  const screenCls = `screen ${type}`;
   return (
     <div className="device-wrapper" style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-        <div className="device" data-device="iPhone7" data-orientation="portrait" data-color="white">
-          <div className="screen" style={{
-          pointerEvents: "auto",
-          overflowY: 'scroll',
-          backgroundColor: socialType.background,
-          display: 'flex',
-          flexDirection: 'column',
-          justifyContent: 'flex-end' }}
-          >
-            {!!image && <Bubble style={{ padding: 0 }} image={image} /> }
-            {!!messages && <Bubble text={messages} bgColor={socialType.bubbleBackground} />}
-            {
-              !!options.length &&
-              options.map(opt => <Bubble
-                style={{ opacity: 0.8, width: '200px', textAlign: 'center' }}
-                text={opt} bgColor={socialType.optionBackground} />)
-            }
+      <div className="device" data-device="iPhone7" data-orientation="portrait" data-color="white">
+        <div className={screenCls}>
+          <Bubble text={messages} image={image} options={options} />
         </div>
-      </div>
-      <div className="button">
       </div>
     </div>
   );
 };
 
-const Bubble = ({bgColor, text, style = {}, image = {}}) => <ScrollContainer style={{
-  fontSize: 10,
-  borderRadius: 8,
-  display: 'flex',
-  flexDirection: 'column',
-  padding: "5px 10px 5px 10px",
-  minHeight: 20,
-  width: "fit-content",
-  margin: '0 40px 8px 8px',
-  backgroundColor: bgColor,
-  wordBreak: 'break-word',
-  whiteSpace: 'pre-wrap',
-  maxWidth: '200px',
-  ...style
-}}
->
-  { image && <img src={image.preview} className="dz-img" alt={image.name} />}
-  <div>{text}</div>
-</ScrollContainer>
+const Bubble = ({text, image = {}, options={}}) => {
+  return <div className="bubles">
+    { Object.keys(image).length > 0 &&
+      <div className="buble buble-image">
+        <h3 className="buble-title">KH Covid-19 Broadcast</h3>
+        <img src={image.preview} className="dz-img" alt={image.name} />
+        </div>
+    }
+    { text !== '' &&
+      <div className="buble buble-message">
+        <h3 className="buble-title">KH Covid-19 Broadcast</h3>
+        <div className="message">{text}</div>
+        {!!options.length &&
+          <div className="poll">
+            <h4 className="buble-sub-title">Poll</h4>
+            {options.map((opt) => {
+              return <div className="option"><span className="icon"></span><span className="text">{opt}</span></div>
+            })}
+          </div>
+        }
+      </div>
+    }
+  </div>
+}
 
 export default PhoneSimulator;
